622f87c937870b57e4eb687a5ab58063a44e8e79
Christian Kuri <christian.kuri.martinez@gmail.com>
2022-08-11 15:59:13 -0600

    add env variables

--- resolveProductionUrl.js
+++ resolveProductionUrl.js
@@ -1,5 +1,5 @@
 const previewSecret = 'SECRET_PREVIEW_KEY'; // Copy the string you used for SANITY_PREVIEW_SECRET
-const projectUrl = 'http://localhost:3000';
+const projectUrl = process.env.SANITY_STUDIO_PREVIEW_URL;
 
 export default function resolveProductionUrl(document) {
   return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`;
